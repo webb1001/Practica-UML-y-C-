a7adbd651b35200f3caf9e9a23d68298bb6b886d
webb1001 <58349946+webb1001@users.noreply.github.com>
2019-12-10 21:44:57 -0600

    Inicio

---
+++ Practica UML/Practica UML/Serie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practica_UML
+{
+    public class Serie
+    {
+        public int Titulo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Ano
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
